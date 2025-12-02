f3caaad11d68c30355e72fc88ad53560cd60650c
metrowave-digital <krclemons97@gmail.com>
2025-12-02 01:54:58 +0100

    redirect fix

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-
-  async redirects() {
-    return [
-      {
-        source: "/pathways",
-        destination: "https://pathways.freedomwc.org",
-        permanent: true,
-      },
-      {
-        source: "/pathways/:slug*",
-        destination: "https://pathways.freedomwc.org/:slug*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
